6874e82a0e7a1de7b9e5e912c04a9ab4f0e92469
Carlos Sevillano <cscmovil@gmail.com>
2021-09-16 10:31:39 +0000

    modif

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -12,7 +12,7 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		String meessage = "cambio 16092021";
+		String meessage = "cambio modificado  16092021";
                 Assert.asserNotNull(meessage);
 	}
 
